6b3bf6e9cb9556484df40f8fc8ab638dd5ae09fb
Arla Figueiredo <arla.figueiredo@gmail.com>
2023-01-06 18:48:01 -0300

    Update message params.

--- message_params.py
+++ message_params.py
@@ -2,9 +2,15 @@ from enum import Enum
 
 
 class MessageType(Enum):
-    CLIENT_SQL_COMMAND = 1
+    SQL_COMMAND = 1
     STAGE = 2
     CONFIRM = 3
     COMMIT = 4
     LEADER_ELECTION = 5
     HEART_BEAT = 6
+
+
+class SenderTypes(Enum):
+    SERVER_RM = 1
+    SERVER_DBM = 2
+    CLIENT = 3
